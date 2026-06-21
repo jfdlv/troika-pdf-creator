28662459c90cdd78ccd4e8c7b5460a75e45ea272
Fernando De La Via <fernando.delavia@coxautoinc.com>
2026-06-21 18:47:16 -0400

    adds changes for deploy to github pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { store } from './store';
 import App from './App';
 import './index.scss';
@@ -10,8 +10,8 @@ const root = createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
