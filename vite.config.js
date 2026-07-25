090503c66847972cd12c04880a19af5c80cbd28c
Fernando De La Via <fernando.delavia@coxautoinc.com>
2026-07-25 19:30:55 -0400

    updated vite.config.js to fix gh-pages issue

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/troika-pdf-creator/',
+  base: '/troika-toolkit/',
   plugins: [react()],
   esbuild: {
     loader: 'jsx',
